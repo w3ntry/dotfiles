f2982fca93d4ae40cf77250675d4038b54e51a6e
W3ntry <187279450+w3ntry@users.noreply.github.com>
2025-09-12 11:43:37 +0200

    first commit
    fix

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+# deps: starship, atuin, zoxide, eza, nvim, trash-cli
+
+if status is-interactive
+    fish_default_key_bindings
+
+    fish_add_path -P ~/.local/bin ~/.cargo/bin ~/.luarocks/bin/
+
+    starship init fish | source
+    atuin init fish --disable-up-arrow | source
+    zoxide init --cmd cd fish | source
+
+    export VISUAL=micro
+    export EDITOR=micro
+
+    abbr l ls
+    abbr rm trash
+
+    alias ltree='eza --icons --hyperlink -T'
+    alias ls='eza --icons --hyperlink -1'
+    alias reload='source ~/.config/fish/config.fish'
+    alias dotfiles='command git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+end
